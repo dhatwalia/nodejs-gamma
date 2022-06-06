72bdb0ac77483329f102e788b2f6d1c08693d189
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2022-06-06 09:20:17 -0400

    Adds date module

--- 01-home.js
+++ 01-home.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var dt = require('./04-modules');
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write("The date and time are currently: " + dt.myDateTime() + "\n");
   res.end('Hello World!');
 }).listen(8080);
 
